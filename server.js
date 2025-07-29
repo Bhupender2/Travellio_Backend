f66f76118fcf58c94e630eb818029e51336991e3
bhupender <b.mehtaa2021@gmail.com>
2025-07-29 18:32:43 +0530

    sercer is added and link is changed here

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 //START SERVER AND CONNECT TO MONGODB
 
-require('dotenv').config({ path: './config.env' })  // To Read .env file and set key value to node environment
+// require("dotenv").config({ path: "./config.env" }); // To Read .env file and set key value to node environment
+require("dotenv").config(); // To Read .env file and set key value to node environment
 
-const app = require('./app')  // Require app.js file
+const app = require("./app"); // Require app.js file
 
-const mongoose = require('mongoose')
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
-mongoose.set('strictQuery', false);  // mongoose new version we have to set this to true or false otherwise will keep givig warning
-mongoose.connect(DB).then(() => console.log("DB CONNECTED SUCCESSFULLY..."))
+const mongoose = require("mongoose");
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.set("strictQuery", false); // mongoose new version we have to set this to true or false otherwise will keep givig warning
+// console.log(process.env.DATABASE);
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB CONNECTED SUCCESSFULLY..."))
+  .catch((err) => console.log("❌ Database connection failed:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening at PORT: ${PORT}...`);
+  console.log(`Server is listening at PORT: ${PORT}...`);
 });
